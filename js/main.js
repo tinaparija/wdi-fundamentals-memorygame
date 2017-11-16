a0a3ebfb501df77f542699282442877ba556099e
tinaparija <tinaparija@gmail.com>
2017-11-15 18:44:25 -0800

    created gamelogic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-var cardOne = "queen"; 
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"]; 
+var cardsInPlay = []; 
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cardOne = cards[0]; 
+cardsInPlay.push(cardOne); 
+console.log("User flipped "+ cardOne);
+
+var cardTwo = cards[2]; 
+cardsInPlay.push(cardTwo);
+console.log("User flipped "+ cardTwo);
+
+
+if (cardsInPlay.length===2){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+}
