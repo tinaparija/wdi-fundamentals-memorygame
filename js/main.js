2caacc9ae2d8a59a0193fb6bab98c05286f5969c
tinaparija <tinaparija@gmail.com>
2017-11-20 15:49:12 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -24,6 +24,8 @@ var cards = [
 var cardsInPlay = []; 
 
 var checkForMatch = function(){
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src',cards[cardId].cardImage);
 	if (cardsInPlay.length===2){
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match!");
@@ -32,15 +34,26 @@ var checkForMatch = function(){
 			alert("Sorry, try again.");
 		}
 	}
-}
+};
 
-var flipCard = function (cardId){
+var flipCard = function (){
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank); 
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);	
-	checkForMatch();
 }; 
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+	for (i=0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id',i);
+		cardElement.addEventListener('click',flipCard);
+		cardElement.addEventListener('click', checkForMatch);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
+
